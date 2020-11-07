958895ea50dab89c0bea520fb2ce2d21c768c460
Carlos Barrios <juanbarrios045@gmail.com>
2020-11-07 03:25:25 -0400

    Master | Fix route

--- src/hooks/ProtectRoute/routes.js
+++ src/hooks/ProtectRoute/routes.js
@@ -22,6 +22,10 @@ export default [
     restricted: true,
   },
   {
+    pathname: "/nuevp-producto",
+    restricted: true,
+  },
+  {
     pathname: "/productos/[id]",
     restricted: true,
   },
